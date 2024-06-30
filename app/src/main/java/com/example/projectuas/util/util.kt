8d47c84a0b18548d65b43d91fffbca5cd985f8a1
hilmyyes <irfansahilmy@gmail.com>
2024-07-01 05:36:02 +0700

    UI Final by Jonathan
    Fix:
    - Tampilan UI
    - Beberapa DataBinding di layout
    - Navigation dan NavBar (Hapus navbar yang ada di login/register).
    
    Bagian ini dikerjakan jonathan tetapi githubnya tidak bisa integrasi ke dalam project ini.

--- app/src/main/java/com/example/projectuas/util/util.kt
+++ app/src/main/java/com/example/projectuas/util/util.kt
@@ -9,47 +9,31 @@ val DB_NAME = "newsdb"
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(db: SupportSQLiteDatabase) {
-        db.execSQL("""
-            CREATE TABLE Users (
-                uuid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-                first_name TEXT NOT NULL, 
-                last_name TEXT NOT NULL, 
-                email TEXT NOT NULL, 
-                username TEXT NOT NULL, 
-                password TEXT NOT NULL, 
-                photo TEXT NOT NULL
-            )
-            """)
-
-        //delete news column from News table
-        db.execSQL(
-            """
-            CREATE TABLE news_new (
-                uuid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-                image TEXT NOT NULL, 
-                title TEXT NOT NULL, 
-                author TEXT NOT NULL, 
-                desc TEXT NOT NULL
-            )
-            """
-        )
-
-        // Copy the data from the old News table to the new table
-        db.execSQL(
-            """
-            INSERT INTO news_new (uuid, image, title, author, desc) 
-            SELECT uuid, image, title, author, desc 
-            FROM News
-            """
-        )
 
         // Drop the old News table
-        db.execSQL("DROP TABLE News")
+        db.execSQL("DROP DATABASE hobbydb")
 
-        // Rename the new table to the old table's name
-        db.execSQL("ALTER TABLE news_new RENAME TO News")
+        // check databases column users
+        db.execSQL( "CREATE TABLE IF NOT EXISTS `Users` (" +
+                "`uuid` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "`firstName` TEXT, " +
+                "`lastName` TEXT, " +
+                "`email` TEXT, " +
+                "`password` TEXT, " +
+                "`photo` TEXT, " +
+                "`username` TEXT)")
+
+        // check databases column news
+        db.execSQL( "CREATE TABLE IF NOT EXISTS `News` (" +
+                "`uuid` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "`image` TEXT, " +
+                "`title` TEXT, " +
+                "`author` TEXT, " +
+                "`desc` TEXT, ")
     }
+
 }
+
 
 fun buildDb(context:Context):NewsDatabase {
     val db = NewsDatabase.buildDatabase(context)
